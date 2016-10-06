cc0b6bf341795c00cdc19221b28dd03b07d32c95
FrankYW_Chen <FrankYW_Chen@compal.com>
2016-10-06 17:04:10 +0800

    using python to write ros service

--- script/dynamic_server.py
+++ script/dynamic_server.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
-
 import roslib
 import rospy
 
 import dynamic_reconfigure.client
+from dynamic_reconfig.srv import *
 
 _name = "/move_base/global_costmap/inflation_layer"
 
 def callback(config):
     rospy.loginfo("Config set to {inflation_radius}, {cost_scaling_factor}, {enabled}".format(**config))
 
+def dynamic_update(req):
+    print "node_name:{}, config_name:{}, new_cnofig:{}".format(req.node_name, req.config_name, req.new_cnofig)
+    return "ok"
+
+def server_test():
+    rospy.init_node('dynamic_reconfig')
+    s = rospy.Service('dynamic_update', DynamicUpdate, dynamic_update)
+    print "Ready to do dynamic_update."
+    rospy.spin()
+
 if __name__ == "__main__":
     try:
-        rospy.init_node("dynamic_client", anonymous=False)
+        server_test()
+        #~ rospy.init_node("dynamic_client", anonymous=False)
 
-        client = dynamic_reconfigure.client.Client(_name, timeout=30, config_callback=callback)
-        pairs = client.get_configuration()
-        for key, value in pairs.iteritems():
-          print "{} : {}".format(key, value)
-
+        #~ client = dynamic_reconfigure.client.Client(_name, timeout=30, config_callback=callback)
+        #~ pairs = client.get_configuration()
+        #~ for key, value in pairs.iteritems():
+          #~ print "{} : {}".format(key, value)
+        #~ client.update_configuration({ 'inflation_radius' : 20})
     #~ r = rospy.Rate(0.1)
     #~ x = 0
     #~ b = False
