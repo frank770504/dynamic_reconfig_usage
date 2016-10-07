5fcc99264e5c59ba866bb9665d0d5ef7455b429b
FrankYW_Chen <FrankYW_Chen@compal.com>
2016-10-07 10:01:30 +0800

    prepare for dynamic_reconfig_common_lib

---
+++ src/dynamic_reconfig_client_test.cpp
@@ -0,0 +1,8 @@
+#include <dynamic_reconfig_common.h>
+#include "ros/ros.h"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "dynamic_reconfig_common_test");
+  ros::NodeHandle n;
+  return 0;
+}
